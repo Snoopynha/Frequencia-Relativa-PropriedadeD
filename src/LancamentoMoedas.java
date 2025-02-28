24e41ecf71e040ea4dc39544757b8f969ea6b04a
Camilly Vitória <vitoriacamilly308@gmail.com>
2025-02-28 18:23:39 -0300

    Não ta dando certo fazer o back pelo VS, to indo para o Eclipse

--- src/LancamentoMoedas.java
+++ src/LancamentoMoedas.java
@@ -1,7 +1,11 @@
 import java.util.Random;
+import spark.Spark;
 
 public class LancamentoMoedas {
     public static void main(String[] args) {
+    	Spark.staticFileLocation("/public");
+    	
+    	Spark
         Random random = new Random();
     }
 }
