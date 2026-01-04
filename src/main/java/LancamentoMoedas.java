8f88ec162cb734b7bdfe70de187ca228c1534e7d
Camilly Vitória <vitoriacamilly308@gmail.com>
2026-01-04 20:55:11 -0300

    fix(backend): ajustar tamanho dos arrays com Math.ceil e aplicar cast double

--- src/main/java/LancamentoMoedas.java
+++ src/main/java/LancamentoMoedas.java
@@ -3,55 +3,62 @@ import spark.Spark;
 import com.google.gson.Gson;
 
 public class LancamentoMoedas {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		Spark.port(4567);
-    	Spark.staticFileLocation("public");
-    	
-    	Spark.get("/lancar", (req,res) -> {
-    		int qtdLancamentos = Integer.parseInt(req.queryParams("qtdLancamentos"));
-    		int intervaloLancamentos = Integer.parseInt(req.queryParams("intervaloLancamentos"));
-    		
-    		int[] caras = new int[qtdLancamentos];
-    		int[] coroas = new int[qtdLancamentos];
-    		int totalCaras = 0;
-    		int totalCoroas = 0;
-    		
-    		Random random = new Random();
-    		
-    		for (int i=0; i<qtdLancamentos; i++) {
-    			if(random.nextBoolean()) {
-    				totalCaras++;
-    			} else {
-    				totalCoroas++;
-    			}
-    			caras[i] = totalCaras;
-    			coroas[i] = totalCoroas;
-    		}
-    		
-    		double[] freqCaras = new double[qtdLancamentos];
-    		double[] freqCoroas = new double[qtdLancamentos];
-    		String[] labels = new String[qtdLancamentos/intervaloLancamentos];
-    		
-    		for(int i=0; i<qtdLancamentos; i+=intervaloLancamentos) {
-    			freqCaras[i/intervaloLancamentos] = (double) caras[i] / (i+1);
-    			freqCoroas[i/intervaloLancamentos] = (double) coroas[i]/ (i+1);
-    			labels[i/intervaloLancamentos] = String.valueOf(i+1);
-    		}
-    		
-    		res.type("application/json");
-            return new Gson().toJson(new Data(labels, freqCaras, freqCoroas));
-    	}); 
-    }
-    
-    static class Data {
-    	String[] labels;
-    	double[] caras;
-    	double[] coroas;
-    	
-    	Data(String[] labels, double[] caras, double[] coroas){
-    		this.labels = labels;
-    		this.caras = caras;
-    		this.coroas = coroas;
-    	}
-    }
+		Spark.staticFileLocation("public");
+
+		Spark.get("/lancar", (req, res) -> {
+			int qtdLancamentos = Integer.parseInt(req.queryParams("qtdLancamentos"));
+			int intervaloLancamentos = Integer.parseInt(req.queryParams("intervaloLancamentos"));
+
+			// Calcula o tamanho exato necessário (arredondando para cima)
+			int tamanhoArray = (int) Math.ceil((double) qtdLancamentos / intervaloLancamentos);
+
+			int totalCaras = 0;
+			int totalCoroas = 0;
+			Random random = new Random();
+
+			// Arrays para armazenar as frequências que serão enviadas
+			double[] freqCaras = new double[tamanhoArray];
+			double[] freqCoroas = new double[tamanhoArray];
+			String[] labels = new String[tamanhoArray];
+
+			int index = 0;
+			for (int i = 0; i < qtdLancamentos; i++) {
+				if (random.nextBoolean()) {
+					totalCaras++;
+				} else {
+					totalCoroas++;
+				}
+
+				// Só armazena no array se for um ponto de intervalo
+				if (i % intervaloLancamentos == 0 && index < tamanhoArray) {
+					freqCaras[index] = (double) totalCaras / (i + 1);
+					freqCoroas[index] = (double) totalCoroas / (i + 1);
+					labels[index] = String.valueOf(i + 1);
+					index++;
+				}
+			}
+
+			java.util.Map<String, Object> resultado = new java.util.HashMap<>();
+			resultado.put("labels", labels);
+			resultado.put("caras", freqCaras);
+			resultado.put("coroas", freqCoroas);
+
+			res.type("application/json");
+			return new Gson().toJson(resultado);
+		});
+	}
+
+	static class Data {
+		String[] labels;
+		double[] caras;
+		double[] coroas;
+
+		Data(String[] labels, double[] caras, double[] coroas) {
+			this.labels = labels;
+			this.caras = caras;
+			this.coroas = coroas;
+		}
+	}
 }
